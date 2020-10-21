32a4a7dd6de79e3dbb595eece4ca97290376a176
evg <rejnea@gmail.com>
2020-10-21 19:43:22 +0300

    edit php

--- www/test/index.php
+++ www/test/index.php
@@ -3,20 +3,8 @@
     include_once'header.php';
 ?>
 
-<div class="jumbotron">
-	<div class="container text-center">
-		<h1></h1>
-        <div id="loreg">
-            <ul class="list-inline">
-                <li>
-                    <a class="btn btn-primary btn-lg" href="login.php">Войти</a>
-                </li>
-                <li>
-                    <a class="btn btn-primary btn-lg" href="register.php">Регистрация</a>
-                </li>
-            </ul>
-        </div>
-	</div>
+<div class="container text-center">
+  fghfghfgh
 </div>
 <div class="container text-center">
 </div>
