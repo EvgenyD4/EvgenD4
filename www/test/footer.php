3fb0992f4119a37cd0a92b234b836d3fa4ae3fbd
evg <rejnea@gmail.com>
2020-10-21 20:00:42 +0300

    img footer

--- www/test/footer.php
+++ www/test/footer.php
@@ -1,3 +1,9 @@
-    <p align=center>lighttpd 2019</p>
+  <div class="footer-img"><img src="footer-bg.jpg" alt="" width="130" height="80"></div>
+   <div class="footer-bg">
+    <div class="copyright">
+    <p><strong>Учебный сайт «Как поймать льва в пустыне»</strong></p>
+    <p>&copy; Влад Мержевич</p>
+   </div>
+  </div>
 </body>
 </html>
